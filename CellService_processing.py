3b16e8e8ec435fd4198e0e58c206db801225c335
Alessia Gallo <81225431+AlessiaGallo@users.noreply.github.com>
2021-03-24 20:39:39 +0100

    Add files via upload

--- CellService_processing.py
+++ CellService_processing.py
@@ -1,6 +1,6 @@
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QRadioButton, QPushButton, QToolTip, QLabel, QVBoxLayout, QDesktopWidget, QStyleFactory)
-from PyQt5.QtWidgets import (QFileDialog, QWidget, QMainWindow, QGroupBox, QAction, QMenu, QSystemTrayIcon, QLineEdit, QTextEdit, QSpinBox, QDoubleSpinBox)
+from PyQt5.QtWidgets import (QDockWidget, QApplication, QWidget, QRadioButton, QPushButton, QToolTip, QLabel, QVBoxLayout, QDesktopWidget, QStyleFactory)
+from PyQt5.QtWidgets import (QCheckBox, QBoxLayout,QFileDialog, QWidget, QMainWindow, QGroupBox, QAction, QMenu, QSystemTrayIcon, QLineEdit, QTextEdit, QSpinBox, QDoubleSpinBox)
 from PyQt5.QtGui import QPixmap, QIcon, QFont, QImage
 from PyQt5 import QtWidgets
 from PyQt5 import QtGui, QtCore
@@ -14,10 +14,9 @@ class CellServiceBinaryProcessing(QMainWindow):
     def __init__(self, parent):
         super().__init__()
         self.parent = parent
-        self.setWindowTitle("Binary processing")
+        
         
         self._createToolBars()
-        self.StatBar = self.statusBar()
         self.init_ui()
 
     def init_ui(self):
@@ -72,7 +71,7 @@ class CellServiceBinaryProcessing(QMainWindow):
         
     def maximize_window(self):
         screen = QDesktopWidget().screenGeometry()
-        self.setFixedSize(int(screen.height()*1.5), int(screen.height()*0.75))
+        self.setFixedSize(int(screen.height()*1.2), int(screen.height()*0.75))
 
     def set_all_images(self):
         self.parent.set_image(self.parent.red_image, self.Original_Label, "red", mask=False)
@@ -81,9 +80,10 @@ class CellServiceBinaryProcessing(QMainWindow):
  
     def _createToolBars(self):
         # Adding a widget to the Edit toolbar
-        editToolBar = QtWidgets.QToolBar("Edit", self)
-        editToolBar.setFixedSize(2000, 100)
-        self.addToolBar(editToolBar)
+        self.setWindowTitle("Binary processing")
+        self.editToolBar = QtWidgets.QToolBar("Edit", self)
+        self.editToolBar.setFixedSize(2000, 100)
+        self.addToolBar(self.editToolBar)
         self.fontSizeSpinBox = QDoubleSpinBox()
         self.fontSizeSpinBox.setFixedSize(80,40)
         self.fontSizeSpinBox.setMaximum(255.000)
@@ -91,18 +91,18 @@ class CellServiceBinaryProcessing(QMainWindow):
         self.fontSizeSpinBox.setDecimals(3)
         self.Label_Min = QLabel('Min Threshold ')
         self.Label_Max = QLabel('Max Threshold ')
-        editToolBar.addWidget(self.Label_Min)
-        editToolBar.addWidget(self.fontSizeSpinBox)
+        self.editToolBar.addWidget(self.Label_Min)
+        self.editToolBar.addWidget(self.fontSizeSpinBox)
         self.fontSizeSpinBox2 = QDoubleSpinBox()
         self.fontSizeSpinBox2.setFixedSize(80,40)
         self.fontSizeSpinBox2.setMaximum(255.000)
         self.fontSizeSpinBox2.setSingleStep(0.1)
         self.fontSizeSpinBox2.setDecimals(3)
-        editToolBar.addSeparator()
-        editToolBar.addWidget(self.Label_Max)
-        editToolBar.addWidget(self.fontSizeSpinBox2)
-        editToolBar.addSeparator()
-        new_btn_ss5 = """QPushButton{background-color: turquoise;
+        self.editToolBar.addSeparator()
+        self.editToolBar.addWidget(self.Label_Max)
+        self.editToolBar.addWidget(self.fontSizeSpinBox2)
+        self.editToolBar.addSeparator()
+        self.new_btn_ss5 = """QPushButton{background-color: turquoise;
                                     border-style: outset;
                                     border-width: 2px;
                                     border-radius: 15px;
@@ -113,29 +113,46 @@ class CellServiceBinaryProcessing(QMainWindow):
                         QPushButton:pressed {background-color: rgb(255, 255, 255);
                                              border-style: inset;}
                      """
+        self.new_btn = """QPushButton{background-color: yellow;
+                                    border-style: outset;
+                                    border-width: 2px;
+                                    border-radius: 15px;
+                                    border-color: yellow;
+                                    font: bold 14px;
+                                    min-width: 5em;
+                                    padding: 6px;}
+                        QPushButton:pressed {background-color: rgb(255, 255, 255);
+                                             border-style: inset;}
+                     """
         self.AutomaticButton = QPushButton('AUTOMATIC THRESHOLD')
-        self.AutomaticButton.setStyleSheet(new_btn_ss5)
+        self.AutomaticButton.setStyleSheet(self.new_btn_ss5)
         self.AutomaticButton.clicked.connect(self.Automatic_threshold)
-        editToolBar.addWidget(self.AutomaticButton)
-        editToolBar.addSeparator()
+        self.editToolBar.addWidget(self.AutomaticButton)
+        self.editToolBar.addSeparator()
         
         self.redRadioButton = QRadioButton('RED')
-        self.redRadioButton.setStyleSheet(new_btn_ss5)
-        editToolBar.addWidget(self.redRadioButton)
-        editToolBar.addSeparator()
+        self.redRadioButton.setStyleSheet(self.new_btn_ss5)
+        self.editToolBar.addWidget(self.redRadioButton)
+        self.editToolBar.addSeparator()
         self.greenRadioButton = QRadioButton('GREEN')
-        self.greenRadioButton.setStyleSheet(new_btn_ss5)
-        editToolBar.addWidget(self.greenRadioButton)
-        editToolBar.addSeparator()
+        self.greenRadioButton.setStyleSheet(self.new_btn_ss5)
+        self.editToolBar.addWidget(self.greenRadioButton)
+        self.editToolBar.addSeparator()
         self.blueRadioButton= QRadioButton('BLUE')
-        self.blueRadioButton.setStyleSheet(new_btn_ss5)
-        editToolBar.addWidget(self.blueRadioButton)
-        editToolBar.addSeparator()
+        self.blueRadioButton.setStyleSheet(self.new_btn_ss5)
+        self.editToolBar.addWidget(self.blueRadioButton)
+        self.editToolBar.addSeparator()
         
         self.Apply = QPushButton('Binarize')
-        self.Apply.setStyleSheet(new_btn_ss5)
+        self.Apply.setStyleSheet(self.new_btn_ss5)
         self.Apply.clicked.connect(self.runIntensityBinarization)
-        editToolBar.addWidget(self.Apply)
+        self.editToolBar.addWidget(self.Apply)
+        
+        self.editToolBar.addSeparator()
+        self.Next = QPushButton('CLICK FOR SEGMENTATION')
+        self.Next.setStyleSheet(self.new_btn)
+        self.Next.clicked.connect(self.segmentation)
+        self.editToolBar.addWidget(self.Next)
     
     def Automatic_threshold(self):
         self.soglia1=0
@@ -176,7 +193,41 @@ class CellServiceBinaryProcessing(QMainWindow):
             self.parent.set_image(self.parent.blue_mask, self.Filtred_Label2, "blue", mask=True)
         else:
             pass
-
+        
+    def segmentation(self):
+        self.setWindowTitle("Segmentation processing")
+        self.removeToolBar(self.editToolBar)
+        self.Filtred_Label.setText(" ")
+        self.Filtred_Label1.setText(" ")
+        self.Filtred_Label2.setText(" ")
+        self.editToolBar2 = QtWidgets.QToolBar("Edit", self)
+        self.editToolBar2.setFixedSize(2000, 100)
+        self.addToolBar(self.editToolBar2)
+        
+        self.Label_Raggio= QLabel('Raggio ')
+        self.editToolBar2.addWidget(self.Label_Raggio)
+        self.Raggio = QDoubleSpinBox()
+        self.Raggio.setFixedSize(80,40)
+        self.editToolBar2.addWidget(self.Raggio)
+        self.editToolBar2.addSeparator()
+        
+        self.removeCheck = QCheckBox("Remove small object")
+        self.editToolBar2.addWidget(self.removeCheck)
+        self.erosionCheck = QCheckBox("Erosion")
+        self.editToolBar2.addWidget(self.erosionCheck)
+        
+        self.editToolBar2.addSeparator()
+        self.Return = QPushButton('CLICK FOR BINARIZE')
+        self.Return.setStyleSheet(self.new_btn)
+        self.Return.clicked.connect(self.binaryproject)
+        self.editToolBar2.addWidget(self.Return)
+        
+        
+    def binaryproject(self):
+        self.removeToolBar(self.editToolBar2)
+        self._createToolBars()
+        
+        
 #PER GIUSEPPE: LE MASCHERE SI CHIAMANO self.parent.colore_mask
 #              MENTRE LE IMMAGINI IN FORMATO NP SONO self.parent.colore_image
 
